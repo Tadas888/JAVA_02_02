2e699075c90740cac21ddbdfbf63c5196d75f97e
Tadas888 <T_zukauskas@yahoo.com>
2018-01-30 14:55:43 +0200

    Antra uzduotis

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("iveskite zodi: ");
+        String zodis = sc.nextLine();
+        System.out.println(zodis.length());
     }
 }
